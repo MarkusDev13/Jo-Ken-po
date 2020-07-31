075e8743b7221b1a7125a03f90a059b53e46c893
markus dennys <marcosdennnnnys@gmail.com>
2020-07-31 15:56:19 -0300

    Alterações

--- js/index.js
+++ js/index.js
@@ -1,16 +1,66 @@
+
+let jogadorEscolha
+
+function escolhaDoJogador(escolha) {
+
+    if (escolha == 0) {
+        jogadorEscolha = "PEDRA"
+    } else if (escolha == 1) {
+        jogadorEscolha = "PAPEL"
+    } else {
+        jogadorEscolha = "TESOURA"
+    }
+
+
+}
+
+
 function jogar() {
 
+    let computadorEscolha = Math.floor(Math.random() * 3)
 
-    let computador = Math.floor(Math.random() * 3)
-    let computadorEscolha
+    
 
-    if (computador == 0) {
+    if (computadorEscolha == 0) {
         computadorEscolha = `PEDRA`
-    } else if (computador == 1) {
+    } else if (computadorEscolha == 1) {
         computadorEscolha = `PAPEL`
     } else {
         computadorEscolha = `TESOURA`
     }
 
-    alert(`A escolha do computador foi ${computadorEscolha}`)
+    let vencedor = "nulo"
+
+
+    if (jogadorEscolha == "PEDRA") {
+        if (computadorEscolha == "PEDRA") {
+            vencedor = "EMPATE"
+        } else if (computadorEscolha == "PAPEL") {
+            vencedor = "COMPUTADOR"
+        } else {
+            vencedor = "JOGADOR"
+        }
+    } else if (jogadorEscolha == "PAPEL") {
+        if (computadorEscolha == "PEDRA") {
+            vencedor = "JOGADOR"
+        } else if (computadorEscolha == "PAPEL") {
+            vencedor = "EMPATE"
+        } else {
+            vencedor = "COMPUTADOR"
+        }
+    } else if (jogadorEscolha == "TESOURA") {
+        if (computadorEscolha == "PEDRA") {
+            vencedor = "COMPUTADOR"
+        } else if (computadorEscolha == "PAPEL") {
+            vencedor = "JOGADOR"
+        } else {
+            vencedor = "EMPATE"
+        }
+    } else {
+        alert ("ERRO INESPERADO")
+    }
+
+    alert(`${jogadorEscolha} ${computadorEscolha} ${vencedor}`)
+    
+
 }
